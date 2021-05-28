40f1d364509eb0602e66c88e8ba13c5b7329f283
cm <cm@CM-PC>
2021-05-28 07:57:19 -0300

    almost done

--- src/content.js
+++ src/content.js
@@ -1,3 +1,85 @@
-const mainInfo = document.createElement("div");
+const homePage = document.createElement("div");
+homePage.id = "home_cont";
+homePage.innerHTML = `
+<img src="/js_restaurant/img/apple0.jpg">
+<div id="intro_cont">
+<h1> Imagine a restaurant... </h1>
+<br/>
+<h3> A restaurant where they would only serve apples. The tasty kind! Not that there's a kind that isn't tasty, but yeah...apples! <br/><br/>
+Big red ones, green ones, small yellow-tinged ones, you name it. None of that orange stuff, they're literally uncomparable, figuratively speaking. <br/><br/>
+</h3> <h2>Anyway, come over to our imaginary place for apple heaven!</h2>
+<br/>
+<h2><i>Everywhere!</i></h2>
+</div>
+`;
 
-export { mainInfo };
+const typeContent = document.createElement("div");
+typeContent.id = "types_cont";
+typeContent.innerHTML = `
+
+<div id='types_text'>
+<h1>TYPES</h1>
+</br>
+<h3>According to wikipedia, there are over 7,500 cultivars of apples. Some are red, others are green, plenty are somewhere in between.</h3> <br/>
+<h3>Althought most are, not all of them are for eating raw, some are too acidic and are used for making ciders or for cooking. </h3> <br/>
+<h3>Some wild varieties called crabapples can't be consumed at all, but are still kept for a variety of reasons, such as great looking trees full of flowers, and for harvesting wood used in smoked goods.</h3>
+</div>
+<img src="/js_restaurant/img/apple3.jpg">
+`;
+
+const infoContent = document.createElement("div");
+infoContent.id = "info_cont";
+infoContent.innerHTML = `
+<img src="/js_restaurant/img/apple1.jpg">
+<div id="info_">
+<h1>Nutritional Info</h1>
+
+<table>
+<tr>
+  <th colspan=3>Portion:  1 Large Apple (~242g)</th>  
+</tr>
+<tr>
+<td>&nbsp;</td>
+</tr>
+<tr>
+    <th>Energy: </th>
+    <td>126 kcal</td>
+</tr>
+<tr>
+<th>Water: </th>
+<td>207g </td>
+</tr>
+<tr>
+<th>Carbs: </th>
+<td>33.4g (11% DV)</td>
+<td>Fibers: 5.8g (23% DV)</td>
+</tr>
+<tr>
+    <th>Protein: </th>
+    <td>&nbsp;</td>
+    <td>0.6g (1% DV)</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+</tr>
+<tr>
+    <th>Vitamin C: </th>
+    
+    <td>11.1mg (19% DV)</td>
+</tr>
+<tr>
+<th>Vitamin K: </th>
+
+    <td>5.3mcg (7% DV)</td>
+</tr>
+<tr>
+    <th>Potassium: </th>
+    
+    <td>259mg (7% DV)</td>
+</tr>
+</table>
+<h3>Source USDA FoodData Central.</h3>
+</div>
+`;
+
+export { homePage, typeContent, infoContent };
--- src/index.js
+++ src/index.js
@@ -1,32 +1,57 @@
-import _ from "lodash";
-import * as mainInfo from "./content.js";
+import * as contentModule from "./content.js";
 
 const dom = {
   container: document.getElementById("content"),
   getDiv: (id, cl = undefined) => {
     const div = document.createElement("div");
     div.id = id;
-    if (cl != undefined) div.classList.add(`${cl}`);
+    if (cl != undefined) div.classList.add(cl);
     return div;
   },
+  getText: (content, id = undefined) => {
+    const p = document.createElement("p");
+    p.innerText = content;
+    if (id != undefined) p.id = id;
+    return p;
+  },
 };
-const bodyElements = ["header", "content", "footer"];
-const tabs = ["header_home", "header_type", "header_info"];
+const bodyElements = ["header", "main", "footer"];
+const tabs = ["home", "types", "info"];
+
 bodyElements.forEach((e) => {
-  const el = dom.getDiv(e);
-  dom.container.appendChild(el);
+  const largerParent = dom.getDiv(e);
+  dom.container.appendChild(largerParent);
   dom[e] = document.getElementById(e);
   if (e == "header") {
+    largerParent.appendChild(dom.getText("APPLES", "header_title"));
     tabs.forEach((t) => {
-      const i = dom.getDiv(t, e);
-      el.appendChild(i);
+      const singleTab = dom.getDiv(t, e);
+      singleTab.innerHTML = `<p>${t.toUpperCase()}</p>`;
+      largerParent.appendChild(singleTab);
+      singleTab.addEventListener("click", swapContent);
     });
   }
   if (e == "footer") {
     const fText = document.createElement("p");
-    fText.innerText = "footer";
-    el.appendChild(fText);
+    fText.innerText = "Made by me.© Images from pexels, data from wikipedia.";
+    largerParent.appendChild(fText);
   }
 });
 
-dom.content.appendChild(mainInfo.mainInfo);
+let lastClick = "";
+function swapContent(e) {
+  let clickTarget = this.id;
+  const contentMatch = {
+    home: contentModule.homePage,
+    types: contentModule.typeContent,
+    info: contentModule.infoContent,
+  };
+  if (lastClick !== "") lastClick.classList.toggle("header_selected");
+  if (lastClick.id !== clickTarget) {
+    lastClick = this;
+    dom.main.innerHTML = "";
+    dom.main.appendChild(contentMatch[clickTarget]);
+    this.classList.toggle("header_selected");
+  }
+}
+dom.main.appendChild(contentModule.homePage);
